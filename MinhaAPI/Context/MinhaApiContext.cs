bab743a9317185c0e3ce874e1b4ca853053aa811
Allan de Melo Heil <allanheil1@gmail.com>
2024-07-20 12:11:56 -0300

    base dbcontext

---
+++ MinhaAPI/Context/MinhaApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaAPI.Models;
+
+namespace MinhaAPI.Context
+{
+    public class MinhaApiContext: DbContext
+    {
+        public MinhaApiContext(DbContextOptions<MinhaApiContext> options) : base (options)
+        {
+        }
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+
+    }
+}
